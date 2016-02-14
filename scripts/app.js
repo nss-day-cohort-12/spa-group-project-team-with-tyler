7eb109b5604d91efa8d1f9c996876b63aaa210f4
Tyler Roberts <steventylerfive@gmail.com>
2016-02-13 20:30:56 -0600

    It really doesn't look pretty, and I'm sure there's a much less complicated way to do it, but the edit feature seems to work

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,8 @@ var checkboxLarge = document.getElementById("checkboxLarge");
 var messageText = '';
 var messageId;
 var oldMessage;
+var messageToBeEdited;
+var paragraphEdit;
 
 clearButton.addEventListener("click", function(){
 	messageContainer.innerHTML = "";
@@ -32,16 +34,34 @@ messageContainer.addEventListener("click", function(e){
 
 inputEl.addEventListener("keyup", function(e){
 	if (e.keyCode == 13) {
-		messageText = inputEl.value;
-		console.log(messageText);
-		Chatty.newMessage("outputField", messageText);
-		inputEl.value = "";
-		clearButton.disabled = false;
-		if (document.getElementsByClassName("message").length > 20) {
-			oldMessage = document.getElementsByClassName("message")[0];
-			oldMessage.parentNode.removeChild(oldMessage);
+		if (inputEl.classList.contains("editMode")) {
+			messageToBeEdited.getElementsByTagName("P")[0].innerHTML = inputEl.value;
+			inputEl.value = "";
+			inputEl.classList.remove("editMode");
+		} else {
+			messageText = inputEl.value;
+			console.log(messageText);
+			Chatty.newMessage("outputField", messageText);
+			inputEl.value = "";
+			clearButton.disabled = false;
+			if (document.getElementsByClassName("message").length > 20) {
+				oldMessage = document.getElementsByClassName("message")[0];
+				oldMessage.parentNode.removeChild(oldMessage);
+			}
 		}
 	}
-})
+});
+
+messageContainer.addEventListener("click", function(e){
+	if (e.target.innerHTML === "Edit") {
+		messageToBeEdited = e.target.parentNode;
+		messageId = messageToBeEdited.id;
+		paragraphEdit = messageToBeEdited.getElementsByTagName("P")[0].innerHTML;
+		inputEl.value = paragraphEdit;
+		inputEl.classList.add("editMode");
+		inputEl.focus();
+		console.log(inputEl.classList);
+	}
+});
 
 Chatty.loadJSON();
