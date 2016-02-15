473f56475b0c17f510694aae942078c14a3380e7
Steven Tyler Roberts <steventylerroberts@Stevens-MacBook-Pro.local>
2016-02-15 14:57:48 -0600

    Added 5 jsons, removed messages.json, reworked xhr.js to load messages in order

--- scripts/xhr.js
+++ scripts/xhr.js
@@ -2,20 +2,71 @@ var Chatty = (function(originalChatty){
 	
 
 	originalChatty.loadJSON = function() {
-		var JSONRequest = new XMLHttpRequest();
+		var JSONRequest1 = new XMLHttpRequest();
+		var JSONRequest2 = new XMLHttpRequest();
+		var JSONRequest3 = new XMLHttpRequest();
+		var JSONRequest4 = new XMLHttpRequest();
+		var JSONRequest5 = new XMLHttpRequest();
 
-		JSONRequest.addEventListener("load", function(){
+		JSONRequest1.addEventListener("load", function(){
 			var messageObject = JSON.parse(this.responseText);
 			var messagesFromJson = messageObject.messages;
-			// console.log(messagesFromJson);
+			console.log(messagesFromJson);
 			messagesFromJson.forEach(function(currentObject){
-				Chatty.newMessage("outputField", currentObject.text, currentObject.timestamp, currentObject.user);
-				//Had to move the private array to the populate page, turns out variables aren't shared as easily as I thought.
+				Chatty.newMessage("outputField", currentObject.text, `${new Date(currentObject.timestamp)}`, currentObject.user);
+			});
+			JSONRequest2.send();
+		})
+
+		JSONRequest2.addEventListener("load", function(){
+			var messageObject = JSON.parse(this.responseText);
+			var messagesFromJson = messageObject.messages;
+			console.log(messagesFromJson);
+			messagesFromJson.forEach(function(currentObject){
+				Chatty.newMessage("outputField", currentObject.text, `${new Date(currentObject.timestamp)}`, currentObject.user);
+			});
+			JSONRequest3.send();
+		})
+
+		JSONRequest3.addEventListener("load", function(){
+			var messageObject = JSON.parse(this.responseText);
+			var messagesFromJson = messageObject.messages;
+			console.log(messagesFromJson);
+			messagesFromJson.forEach(function(currentObject){
+				Chatty.newMessage("outputField", currentObject.text, `${new Date(currentObject.timestamp)}`, currentObject.user);
+			});
+			JSONRequest4.send();
+		})
+
+		JSONRequest4.addEventListener("load", function(){
+			var messageObject = JSON.parse(this.responseText);
+			var messagesFromJson = messageObject.messages;
+			console.log(messagesFromJson);
+			messagesFromJson.forEach(function(currentObject){
+				Chatty.newMessage("outputField", currentObject.text, `${new Date(currentObject.timestamp)}`, currentObject.user);
+			});
+			JSONRequest5.send();
+		})
+
+		JSONRequest5.addEventListener("load", function(){
+			var messageObject = JSON.parse(this.responseText);
+			var messagesFromJson = messageObject.messages;
+			console.log(messagesFromJson);
+			messagesFromJson.forEach(function(currentObject){
+				Chatty.newMessage("outputField", currentObject.text, `${new Date(currentObject.timestamp)}`, currentObject.user);
 			});
 		})
 
-		JSONRequest.open("GET", "../json/messages.json");
-		JSONRequest.send();
+		JSONRequest1.open("GET", "../json/message1.json");
+		JSONRequest1.send();
+
+		JSONRequest2.open("GET", "../json/message2.json");
+
+		JSONRequest3.open("GET", "../json/message3.json");
+
+		JSONRequest4.open("GET", "../json/message4.json");
+
+		JSONRequest5.open("GET", "../json/message5.json");
 	}
 
 	return originalChatty;
